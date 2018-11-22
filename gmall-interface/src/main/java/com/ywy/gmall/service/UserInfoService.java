5b77758faf389f376c6d3d4e18979011e9417546
yiwenyuan <1027553948@qq.com>
2018-11-22 10:12:40 +0800

    创建项目

---
+++ gmall-interface/src/main/java/com/ywy/gmall/service/UserInfoService.java
@@ -0,0 +1,13 @@
+package com.ywy.gmall.service;
+
+import com.ywy.gmall.bean.UserInfo;
+
+import java.util.List;
+
+public interface UserInfoService {
+    // alt+enter
+    List<UserInfo> findAll();
+
+    // 根据userId 查询用户地址列表
+    //List<UserAddress> getUserAddressList(String userId);
+}
